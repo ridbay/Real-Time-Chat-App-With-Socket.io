70e912d0d81b775dc728fdf0ddccf8e04a01dd76
Ridwan Balogun <balogunridwan@gmail.com>
2020-10-17 17:41:01 +0100

    Joining the conversation

--- client/app.js
+++ client/app.js
@@ -1,2 +1,25 @@
 const socket = io();
-kh
+
+const dom = {
+    nameInput: document.querySelector('.name-input'),
+    joinButton: document.querySelector('.join'),
+    inputAvatar: document.querySelector('.messaging-form .avatar'),
+    welcomeMessage: document.querySelector('h1'),
+    feed: document.querySelector('.feed'),
+    feedback: document.querySelector('.feedback')
+};
+
+const user = {
+    name: null,
+    avatar: null
+};
+
+dom.joinButton.onclick = e => {
+    e.preventDefault();
+
+    if(!dom.nameInput.value){
+        dom.nameInput.parentElement.classList.add("error");
+    }else{
+        enterChannel();
+    }
+}
