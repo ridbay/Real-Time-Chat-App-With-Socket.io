255da9aa6291eb35808557c6164932c31e6f95bc
Ridwan Balogun <balogunridwan@gmail.com>
2020-10-17 17:36:54 +0100

    Initialize socket.io in client and added dom object

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const app     = express();
-const http    = require('http').createServer(app);
-const io      = require('socket.io')(http);
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
-app.use(express.static(__dirname + '/../client'));
+app.use(express.static(__dirname + "/../client"));
 
 http.listen(3000);
 
-console.log('Server is listening on http://localhost:3000');
+console.log("Server is listening on http://localhost:3000");
 
-io.on('connection', socket => {
-    console.log('connected...');
+io.on("connection", (socket) => {
+  console.log("connected...");
 });
