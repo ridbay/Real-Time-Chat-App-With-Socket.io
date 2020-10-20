b85ef3be423037bf584814d2c58b4c1e5d18b5ec
Ridwan Balogun <balogunridwan@gmail.com>
2020-10-20 18:39:56 +0100

    Adding welcome messages

--- client/app.js
+++ client/app.js
@@ -1,47 +1,72 @@
 const socket = io();
 
 const dom = {
-    nameInput: document.querySelector('.name-input'),
-    joinButton: document.querySelector('.join'),
-    inputAvatar: document.querySelector('.messaging-form .avatar'),
-    welcomeMessage: document.querySelector('h1'),
-    feed: document.querySelector('.feed'),
-    feedback: document.querySelector('.feedback')
+  nameInput: document.querySelector(".name-input"),
+  joinButton: document.querySelector(".join"),
+  inputAvatar: document.querySelector(".messaging-form .avatar"),
+  welcomeMessage: document.querySelector("h1"),
+  feed: document.querySelector(".feed"),
+  feedback: document.querySelector(".feedback"),
 };
 
 const user = {
-    name: null,
-    avatar: null
+  name: null,
+  avatar: null,
 };
 
-dom.joinButton.onclick = e => {
-    e.preventDefault();
+dom.joinButton.onclick = (e) => {
+  e.preventDefault();
 
-    if(!dom.nameInput.value){
-        dom.nameInput.parentElement.classList.add("error");
-    }else{
-        enterChannel();
-    }
-}
+  if (!dom.nameInput.value) {
+    dom.nameInput.parentElement.classList.add("error");
+  } else {
+    enterChannel();
+  }
+};
 
 const enterChannel = () => {
-    const avatar = getAvatar();
-    const name = dom.nameInput.value;
+  const avatar = getAvatar();
+  const name = dom.nameInput.value;
 
-    dom.joinButton.remove();
-    dom.welcomeMessage.remove();
+  dom.joinButton.remove();
+  dom.welcomeMessage.remove();
 
+  dom.nameInput.value = "";
+  dom.nameInput.placeholder = "Send a message for the channel ....";
 
-    dom.nameInput.value = "";
-    dom.nameInput.placeholder = "Send a message for the channel ....";
+  dom.inputAvatar.innerText = "";
+  dom.inputAvatar.getElementsByClassName.backgroundImage = avatar;
+  dom.inputAvatar.getElementsByClassName.backgroundSize = "contain";
 
+  user.name = name;
+  user.avatar = avatar;
 
-    dom.inputAvatar.innerText= "";
-    dom.inputAvatar.getElementsByClassName.backgroundImage = avatar;
-    dom.inputAvatar.getElementsByClassName.backgroundSize = "contain";
+  addWelcomeMessage({ avatar }, true);
+};
 
-    user.name = name;
-    user.avatar = avatar;
+const getAvatar = () => {
+  const size = Math.floor(Math.random() * 100) + 25;
+  return `url(https://www.placeage.com/${size}/${size})`;
+};
 
-    addWelcomeMessage({avatar}, true);
-}
+const addWelcomeMessage = (user, you) => {
+  const welcomeMessage = document.createElement("li");
+  const message = you
+    ? "You have joined the conversation"
+    : `<span class="user-name">${user.name}</span> has joined the conversation`;
+
+  const avatar = you
+    ? ""
+    : `<span class="avatar" style="background:${user.avatar}; background-size: contain;"></span>`;
+
+  welcomeMessage.classList = "welcome-message";
+  welcomeMessage.innerHTML = `
+    <hr />
+    <div class="welcome-message-text">
+    ${avatar}
+    ${message}
+    </div>
+    `;
+
+  dom.feed.appendChild(welcomeMessage);
+};
